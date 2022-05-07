6d55cc81785236406bbe7847eb6b36656c1f60f9
Eid Azzam <eidmahmoud134@gmail.com>
2022-05-07 19:26:16 +0200

    fully project

---
+++ src/Components/MenuContainer.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const MenuContainer = ( {icon}) => {
+  return (
+    <div>
+     <div className="iconContainer">{icon}</div>
+    </div>
+  )
+}
+
+export default MenuContainer
